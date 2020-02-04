b7d254ab91bb90be7a72f34146035c7a8249d8f7
Tanja Veljkovic <veljkovictanja@gmail.com>
2020-02-04 20:21:16 +0100

    Adds age cheker

--- src/main/java/main/helloworld/Main.java
+++ src/main/java/main/helloworld/Main.java
@@ -1,5 +1,7 @@
 package main.helloworld;
 
+import java.util.Scanner;
+
 /**
  *
  * @author qa
@@ -9,9 +11,57 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Hello World!");
         System.out.println("Tanja Veljkovic");
+        
+        int age= 5;
+        if(age > 18) {
+            System.out.println("Osoba je punoletna.");
+           
+        } else {
+        
+        System.out.println("Osoba je maloletna.");
     }
+        System.out.println("Unesite broj godina: ");
+        Scanner input = new Scanner(System.in);
+        int userAge = input.nextInt();
+        System.out.println("Korisnik je uneo: " + userAge);
+    } 
     
+    
+    int Personage = 7;
+        if(Personage < 5) {
+            System.out.println("Beba");
+         }
+            if(Personage > 6<11)
+            {
+            System.out.println("Kid");
+          
+          }
+            
+      
+           if(Personage > 12<17) {
+            System.out.println("Teen");
+          
+          }  
+           
+           
+           if(Personage > 18) {
+            System.out.println("Teen");
+           
+           
+        
+           
+         else {
+        
+        System.out.println("Osoba je Invalid");
+    }
+        System.out.println("Unesite broj godina: ");
+        Scanner input = new Scanner(System.in);
+        int userAge = input.nextInt();
+        System.out.println("Korisnik je uneo: " + userAge);
+    } 
 }
+
